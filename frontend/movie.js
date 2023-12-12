707f1598e78947da8b46a549b18a60e2e862e52a
paragw3dev <152947223+paragw3dev@users.noreply.github.com>
2023-12-13 00:27:57 +0530

    Update movie.js

--- frontend/movie.js
+++ frontend/movie.js
@@ -2,7 +2,7 @@ const url = new URL(location.href);
 const movieId = url.searchParams.get("id")
 const movieTitle = url.searchParams.get("title")
 
-const APILINK = 'https://review-backend.beaucarnes.repl.co/api/v1/reviews/';
+const APILINK = 'http://localhost:8000/api/v1/reviews/';
 
 
 const main = document.getElementById("section");
